5a1da9812c1d18831e90e0db27a081ea5d2554fd
Md Minhazul Haque <mdminhazulhaque@gmail.com>
2016-03-07 13:09:11 +0600

    first commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd /Library/Keyboard\ Layouts
+sudo rm -f Probhat.keylayout
+sudo rm -f Probhat.icns
+sudo curl -O# https://raw.githubusercontent.com/minhazul-haque/Probhat/master/Probhat.keylayout
+sudo curl -O# https://raw.githubusercontent.com/minhazul-haque/Probhat/master/Probhat.icns
