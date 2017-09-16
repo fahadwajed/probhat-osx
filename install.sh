74a87c2659d58c1ef2dd813103d43a3cfc248420
Md. Minhazul Haque <minhazul-haque@users.noreply.github.com>
2017-09-16 20:11:23 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 cd /Library/Keyboard\ Layouts
 sudo rm -f Probhat.keylayout
 sudo rm -f Probhat.icns
-sudo curl -O# https://raw.githubusercontent.com/minhazul-haque/Probhat/master/Probhat.keylayout
-sudo curl -O# https://raw.githubusercontent.com/minhazul-haque/Probhat/master/Probhat.icns
+sudo curl -O# https://raw.githubusercontent.com/minhazul-haque/probhat-osx/master/Probhat.keylayout
+sudo curl -O# https://raw.githubusercontent.com/minhazul-haque/probhat-osx/master/Probhat.icns
